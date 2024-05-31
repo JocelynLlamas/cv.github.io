3029d7c2e0919ec4160b185c5ec6be978856aee4
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-05-30 20:18:05 -0600

    profile

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Logo from '../images/logo.jpg'
+import Logo from '../images/profile.jpg'
 import ThemeToggle from './ThemeToggle.vue'
 </script>
 
@@ -12,8 +12,8 @@ import ThemeToggle from './ThemeToggle.vue'
         <div class="shrink-0 mr-4 lg:fixed">
           <!-- Logo -->
           <router-link class="flex items-center space-x-4" to="/" aria-label="Cruip">
-            <img class="rounded-full" :src="Logo" width="32" height="32" alt="Mary Rutt">
-            <span class="font-caveat text-xl text-slate-200">Mary Rutt</span>
+            <img class="rounded-full" :src="Logo" width="50" height="50" alt="Jocelyn">
+            <span class="font-caveat text-xl text-slate-200">Jocelyn Llamas</span>
           </router-link>
         </div>
 
